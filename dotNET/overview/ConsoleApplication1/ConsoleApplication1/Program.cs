e9873f79c8a94d7b008b816112145121982f31fe
DaniloVieiraCosta1729 <75656191+DaniloVieiraCosta1729@users.noreply.github.com>
2022-01-22 09:41:53 -0300

    overview C#

---
+++ dotNET/overview/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int a = 3;
+            int b = 3;
+            int c = a + b;
+            int d = a * b;
+            double e = Math.Sqrt(a * b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+             */
+            double a;
+            double b;
+            Console.WriteLine("Esse código retorna a raiz quadrada de um número positivo. \nDigite um número positivo: ");
+            a = double.Parse(Console.ReadLine());
+            b = Math.Sqrt(a);
+            Console.WriteLine(b);
+
+            Console.ReadLine();
+        }
+    }
+}
