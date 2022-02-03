7d5699df1b9c8c359bcb1fbd84795998106a01bd
Danilo Vieira Costa <75656191+DaniloVieiraCosta1729@users.noreply.github.com>
2022-02-03 18:47:24 -0300

    Exercícios C#
    Solução dos exercícios da playlist de Python do Guanabara, porém, resolvidos em C#.

---
+++ Exercicios_de_Csharp/Exercicios_de_Csharp/exercicio1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios_de_Csharp
+{
+    public class exercicio1
+    {
+        public static void Exercicio_1()
+        {
+            Console.WriteLine("Olá. Qual é o seu nome? ");
+            string nome = Console.ReadLine();
+            Console.WriteLine($"Prazer em te conhecer, {nome}!");
+            Console.ReadLine();
+        }
+    }
+}
