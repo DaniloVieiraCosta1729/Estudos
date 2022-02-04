ef6fcadd3d460a9114ef43d712363d9655dd94ad
DaniloVieiraCosta1729 <75656191+DaniloVieiraCosta1729@users.noreply.github.com>
2022-02-03 23:43:07 -0300

    Exercicios

---
+++ Exercicios_de_Csharp/Exercicios_de_Csharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios_de_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            exercicio3.Exercicio_3();
+        }
+    }
+}
