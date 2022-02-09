88b0afa33849575ea2d525d7bf8ce082b92a5a4d
DaniloVieiraCosta1729 <75656191+DaniloVieiraCosta1729@users.noreply.github.com>
2022-02-09 17:48:58 -0300

    Passagem de Referencia.

---
+++ dotNET/aula25/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class aula25
+{
+    static void Main(string[] args)
+    {
+        int f8 = 8;
+        fib(ref f8);
+        System.Console.WriteLine(f8);
+    }
+
+    static int fib(ref int n)
+    {
+        return n *= 2;
+    }
+}
